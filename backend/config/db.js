438549ab36e13093c5714d79165b216537faf0ef
Dungsora04 <tuandung040204@gmail.com>
2024-06-20 12:19:00 +0700

    add, show, remove food from db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://bobbyspencer:3871249@cluster0.vhulhdq.mongodb.net/food-delivery').then(()=>console.log("Database connected"));
-}
+  await mongoose
+    .connect(
+      "mongodb+srv://bobbyspencer:3871249@cluster0.vhulhdq.mongodb.net/food-delivery"
+    )
+    .then(() => console.log("Database connected"));
+};
