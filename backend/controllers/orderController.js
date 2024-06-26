e4538a0f47455a82dd5bc7a8d8df59bf7bcde79a
Dungsora04 <tuandung040204@gmail.com>
2024-06-26 21:47:17 +0700

    fix error

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -3,6 +3,7 @@ import userModel from "../models/userModel.js";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+// backup code: vblx-nqbx-oche-bvka-fjjp
 
 // placing user order for frontend
 const placeOrder = async (req, res) => {
