f4d5eb70e6b580006ac4be3b28ded51aec4223bf
Dungsora04 <tuandung040204@gmail.com>
2024-06-26 22:10:41 +0700

    delete stripe secret

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,8 +2,7 @@ import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-// backup code: vblx-nqbx-oche-bvka-fjjp
+//const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placing user order for frontend
 const placeOrder = async (req, res) => {
