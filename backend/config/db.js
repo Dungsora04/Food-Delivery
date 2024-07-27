32ca37d91b6713d1eb5d3d353a5693704fd6950b
Dungsora04 <tuandung040204@gmail.com>
2024-07-27 12:01:35 +0700

    minor change

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://bobbyspencer:3871249@cluster0.vhulhdq.mongodb.net/food-delivery"
+      `${process.env.MONGODB_URI}/food-delivery`
     )
     .then(() => console.log("Database connected"));
 };
